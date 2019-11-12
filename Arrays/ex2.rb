a7b1c36a00c94ed372107fd8abb88d68bb778347
Dan <dnhcode@protonmail.com>
2019-11-11 16:29:28 -0800

    Add exercises for Arrays

---
+++ Arrays/ex2.rb
@@ -0,0 +1,10 @@
+# In the code below, an array containing different types of pets is assigned to
+# pets.
+
+pets = ['cat', 'dog', 'fish', 'lizard']
+# Select 'fish' and 'lizard' from pets at the same time, assign the return value
+# to a variable named my_pets, then print the value of my_pets.
+
+my_pets = pets[2] + ', ' + pets[3]
+
+puts my_pets

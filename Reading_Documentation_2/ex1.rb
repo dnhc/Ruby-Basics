150dd4f3e30f90aa3e1b2bcbfa57da590d7c3b78
Dan <dnhcode@protonmail.com>
2019-11-11 16:24:17 -0800

    Add exercises for Reading Documentation 2

---
+++ Reading_Documentation_2/ex1.rb
@@ -0,0 +1,4 @@
+# How would you use String#upcase to create an uppercase version of he string
+# "xyz"?
+
+'xyz'.upcase
---
+++ Reading_Documentation_2/ex3.rb
@@ -0,0 +1,14 @@
+# Assume you have the following code
+
+s = 'abc def ghi,jkl mno pqr,stu vwx yz'
+puts s.split.inspect
+puts s.split(',').inspect
+puts s.split(',', 2).inspect
+
+# What will each of the 3 puts statements print?
+
+# ["abc", "def", "ghi,jkl", "mno", "pqr,stu", "vwx", "yz"]
+
+# ["abc def ghi", "jkl mno pqr", "stu vwx yz"]
+
+# ["abc def ghi", "jkl mno pqr,stu vwx yz"]
